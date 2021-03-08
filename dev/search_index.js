de1f79d000f6b227f66ea0d9ca34f75aeab8371e
Documenter.jl <documenter@juliadocs.github.io>
2021-03-08 18:26:04 +0000

    build based on 219a444

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Demo.jl","page":"Demo.jl","title":"Demo.jl","text":"","category":"section"},{"location":"","page":"Demo.jl","title":"Demo.jl","text":"Documentation for Demo.jl","category":"page"}]
+}
